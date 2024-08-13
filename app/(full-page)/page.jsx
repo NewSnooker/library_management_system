1df615231c148533353db79cb87c77677a7726a1
NewSnooker <nu0075533@gmail.com>
2024-08-13 17:30:37 +0700

    fix "/"

--- app/(full-page)/page.jsx
+++ app/(full-page)/page.jsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="relative ">
+    <div className="relative mt-8">
       <ParticlesDemo />
-      <Link href="/home">
+      <Link href="/home" className="px-4 sm:px-60">
         <ShinyButton
-          className=" ml-10 absolute top-96 sm:top-[420px]  border border-custom-border text-custom-text"
+          className=" ml-10  absolute top-96 sm:top-[420px]  border border-custom-border text-custom-text"
           text="อ่านเพิ่มเติม "
         />
       </Link>

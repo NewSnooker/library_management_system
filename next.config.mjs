e0fb0947df52ae79e5ecf39839afb3f625ec018f
NewSnooker <nu0075533@gmail.com>
2024-08-28 19:30:25 +0700

    fix next.config

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,28 @@ const nextConfig = {
       "fakeimg.pl",
     ], // Add other domains as needed
   },
+  async headers() {
+    return [
+      {
+        // Match all routes
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+          {
+            key: 'CDN-Cache-Control',
+            value: 'no-store',
+          },
+          {
+            key: 'Vercel-CDN-Cache-Control',
+            value: 'no-store',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;

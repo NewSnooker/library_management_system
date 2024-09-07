cc507667bb3017724ecca684ca574a6d589c3ef5
NewSnooker <nu0075533@gmail.com>
2024-09-07 14:58:29 +0700

    setTimeout in my book user

--- app/(front-end)/books/my-books/page.jsx
+++ app/(front-end)/books/my-books/page.jsx
@@ -2,22 +2,34 @@
 import HeadTitleBreadcrumb from "@/components/frontend/HeadTitleBreadcrumb";
 import { Album, BookCheck, BookText } from "lucide-react";
 import { useSession } from "next-auth/react";
-import React from "react";
+import React, { useEffect } from "react";
 import DataTable from "@/components/backoffice/data-table-components/DataTable";
 import { getData } from "@/lib/getData";
 import { columns } from "./columns";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 
 export default function page() {
   const { data: session, status } = useSession();
   const userId = session?.user?.id;
-  console.log(userId);
+  const router = useRouter();
+
   const { data: borrows, isLoading: isBorrowsLoading } = useQuery({
     queryKey: ["borrows_my_books_borrower", userId],
     queryFn: () => getData(`all/borrows/user/${userId}`),
     enabled: !!userId,
   });
+
+ useEffect(() => {
+    const timer = setTimeout(() => {
+      if (!userId) {
+        router.push("/login");
+        router.refresh();
+      }
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, [userId]);
 
   return (
     <div className="">
